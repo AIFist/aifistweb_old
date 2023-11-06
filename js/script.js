a53f5b6bff3e964fb890a55030a564ba49938ca8
Anshalali <anshalali73@gmail.com>
2023-11-06 16:18:35 +0500

    enh: add some css work in post boxs

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.p-wrapper').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+
+    $('.p-wrapper').slick({
+        slidesToShow: 1,
+        slidesToScroll: 2,
+        autoplay: true,
+    });
+  });
