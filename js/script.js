f3cd158eb55af0165003daf8758407d958212ce6
Anshalali <anshalali73@gmail.com>
2023-11-28 13:42:52 +0500

    enh: i have done card animation

--- js/script.js
+++ js/script.js
@@ -1,100 +1,23 @@
-
-// $(document).ready(function(){
-//     $('.p-wrapper').slick({
-//       slidesToShow: 3,
-//       slidesToScroll: 1,
-//       autoplay: true,
-//       autoplaySpeed: 2000,
-//       nextArrow: $('.next'),
-//       prevArrow: $('.prev')
-//     });
-
-//     // $('.p-wrapper').slick({
-//     //     slidesToShow: 1,
-//     //     slidesToScroll: 2,
-//     //     autoplay: true,
-//     // });
-//   });
-
-  // $(document).ready(function(){
-  //     $('.p-wrapper').slick({
-  //       slidesToShow: 3,
-  //       slidesToScroll: 1,
-  //       autoplay: true,
-  //       autoplaySpeed: 2000,
-  //       nextArrow: $('.next'),
-  //       prevArrow: $('.prev'),
-  //       // Add custom animation
-  //       cssEase: 'ease-in-out',
-  //       speed: 1000
-  //     });
-  // });
-
-  // $(document).ready(function(){
-  //       $('.p-wrapper').slick({
-  //         slidesToShow: 3,
-  //         slidesToScroll: 1,
-  //         autoplay: true,
-  //         autoplaySpeed: 2000,
-  //         nextArrow: $('.next'),
-  //         prevArrow: $('.prev'),
-  //         // Add custom animation
-  //         cssEase: 'ease-in-out',
-  //         speed: 1000,
-  //         fade: true,
-  //         // TODO Add other custom options for card animation 
-  //       });
-  //   });
-// $(document).ready(function(){
-//     $('.p-wrapper').slick({
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         autoplaySpeed: 2000,
-//         nextArrow: $('.next'),
-//         prevArrow: $('.prev'),
-//         // Add custom animation
-//         cssEase: 'ease-in-out',
-//         speed: 1000,
-//         fade: true,
-//         // TODO Add other custom options for card animation 
-//         // TODO Add other custom options for card animation here
-//     });
-// });
-// $(document).ready(function(){
-//     $('.p-wrapper').slick({
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         autoplaySpeed: 2000,
-//         nextArrow: $('.next'),
-//         prevArrow: $('.prev'),
-//         // Add custom animation
-//         cssEase: 'ease-in-out',
-//         speed: 1000,
-//         fade: true,
-//         // TODO Add other custom options for card animation 
-//         // TODO Add other custom options for card animation here
-//         // TODO Implement custom options for card animation
-//     });
-// });
-
 $(document).ready(function(){
     $('.p-wrapper').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
         nextArrow: $('.next'),
         prevArrow: $('.prev'),
         cssEase: 'ease-in-out',
-        speed: 1000,
-        cssEase: 'ease-in-out',
-        speed: 1000,
+        speed: 1080,
         fade: false,
-        // TODO Add other custom options for card animation 
-        // TODO Add other custom options for card animation here
-        // TODO Implement custom options for card animation
-        // TODO Find a custom card on the internet and replace the default card with it
+        focus: 'center',
+        centerMode: true, // Enable center mode
+        centerPadding: '0', // Adjust padding as needed
     });
+    // Add this inside your document ready function
+  $('.p-wrapper').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    $('.p-box').removeClass('center blur');
+    $('.p-box[data-slick-index="' + nextSlide + '"]').addClass('center');
+    $('.p-box[data-slick-index="' + (nextSlide - 1) + '"]').addClass('blur');
+    $('.p-box[data-slick-index="' + (nextSlide + 1) + '"]').addClass('blur');
+  });
 });
